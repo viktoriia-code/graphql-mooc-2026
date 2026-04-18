6b67efd23e20e27401081c84d3016f945ed05bc6
Viktoriia <beloborodova.viktoriia@gmail.com>
2026-04-18 22:09:33 +0300

    Solved 8.9

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,9 +1,29 @@
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+      id
+    }
+  }
+`
+
 const Books = (props) => {
   if (!props.show) {
     return null
   }
 
-  const books = []
+  const result = useQuery(ALL_BOOKS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  
+  const books = result.data.allBooks
 
   return (
     <div>

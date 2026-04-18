7e743dd49e9df37fda00f6e8d3af3cf6388ce82d
Viktoriia <beloborodova.viktoriia@gmail.com>
2026-04-18 18:25:39 +0300

    Solved 8.6

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { assertNullableType } = require('graphql')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -120,6 +121,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book!
+  }
 `
 
 const resolvers = {
@@ -140,6 +150,16 @@ const resolvers = {
   Author: {
     bookCount: (author) => {
       return books.filter(book => book.author === author.name).length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find(a => a.name === args.author)) {
+        authors = authors.concat({ name: args.author, id: uuid() })
+      }
+      return book
     }
   }
 }
